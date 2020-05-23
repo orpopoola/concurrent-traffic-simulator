6ac1c576e7a29b60ae264f77978d8cd95fa478df
orpopoola <olaoluwapopoola@gmail.com>
2020-05-24 00:59:57 +0100

    end of task 5

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -15,6 +15,11 @@ T MessageQueue<T>::receive()
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
+    std::unique_lock<std::mutex> lock(_mutex);
+    _condition.wait(lock, [this]{ return  !_queue.empty();});
+    T msg = std::move(_queue.front());
+    _queue.pop_front();
+    return msg;
 }
 
 template <typename T>
@@ -30,7 +35,7 @@ void MessageQueue<T>::send(T &&msg)
 
 /* Implementation of class "TrafficLight" */
 
-/* 
+
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
@@ -41,8 +46,14 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+    while(true)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        auto msg = _queue.receive();
+        if(msg == green){return;}
+    }
 }
-*/
+
 TrafficLightPhase TrafficLight::getCurrentPhase()
 {
     return _currentPhase;
@@ -65,7 +76,8 @@ void TrafficLight::cycleThroughPhases()
     int cycle = 4000 + rand() % 2001;
     auto startTime = std::chrono::system_clock::now();
     while(true)
-    {
+    {        
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));//one second delay
         std::chrono::nanoseconds waitTime = std::chrono::system_clock::now() - startTime; //.now() is in ns, we later cast to ms
         int waitTimeInt = (std::chrono::duration_cast<std::chrono::milliseconds>(waitTime)).count();
         if(waitTimeInt>cycle)
